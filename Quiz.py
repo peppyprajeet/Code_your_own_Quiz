d3b65504366a2f340e8a8d42061f859f9a207b20
Prajeet Kumar Patibandla <peppyprajeet@users.noreply.github.com>
2017-05-21 22:54:15 -0400

    Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,28 +1,7 @@
+# -*- coding: cp1252 -*-
 # IPND Stage 2 Final Project
 
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
-
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-#-----------#
-#-Questions-#
-#-----------#
-
-#Prompt Question
+#Questions
 questions = ["""What should be filled in for __1__?""",
              """What should be filled in for __2__?""",
              """What should be filled in for __3__?""",
@@ -31,86 +10,77 @@ questions = ["""What should be filled in for __1__?""",
 holders = ['__1__','__2__','__3__','__4__']
 
 #Quiz Templates
-quiz_text = {'easy':"""***The current paragraph reads as such***
+quiz_text = {'easy':"""***Read the Paragraph and Fill in the blanks as needed***
 
 Programming Language starts with basic output, 'Hello __1__!'
-
 In __2__ this is easy;
-
 Syntax for this is: __3__ 'Hello __1__!'
+In __2__ a function is created with __4__ keyword.""",
 
-A__4__is created with def keyword.""",
 
+'medium':"""***Read the Paragraph and Fill in the blanks as needed***
 
-'medium':"""***The current paragraph reads as such***
-
-A __1__ is created with the def keyword.  You specify the inputs a
-__1__ takes by adding __2__ separated by commas between the parentheses.
+A __1__ is created with the def keyword.
+You specify the inputs a __1__ takes by adding __2__ separated by commas between the parentheses.
 __1__s by default returns __3__ if you don't specify the value to retrun.
 __2__ can be standard data types such as string, integer, dictionary, tuple,
 and __4__ or can be more complicated such as objects and lambda functions.""",
 
-'hard':"""***The current paragraph reads as such***
+'hard':"""***Read the Paragraph and Fill in the blanks as needed***
 
-Artificial __1__ (A.I.)has become a 'buzz word' of publications everywhere.
+Machine learning is the subfield of computer science that, according to Arthur Samuel in 1959,
+gives "computers the ability to learn without being explicitly programmed."
 
-However to better understand A.I. we'll dive into two topics.
-The first is Computer __2__ (CV) which allows a computer to 'see'
-and label what's in the computers surroundings. The second is
-Natural __3__ Processing (NLP) which allows computers to understand
-voice commands as well as speak back to the user.
+Evolved from the study of __1__ recognition and computational learning theory in artificial intelligence,
+machine learning explores the study and construction of __2__ that can learn from and make predictions
+on data – such __2__ overcome following strictly static program instructions by making data-driven predictions or decisions,
+through building a model from sample inputs.
 
-All of this is now becoming possible due to Deep __4__ (DL)
-and Machine __4__ (ML).
+Machine learning is employed in a range of computing tasks where designing and programming
+explicit __2__ with good performance is difficult or unfeasible;example applications include email filtering,
+detection of network intruders or __3__ insiders working towards a data breach,__4__ character recognition (OCR),learning to rank and computer vision.
 """}
 
-#---Answers to Quizes---#
-answers = {'easy':['World','python','print','function'],
+#Answers to Quizes
+answers = {'easy':['World','python','print','def'],
            'medium':['function','arguments','None','list'],
-           'hard':['Intelligence','Vision','Language','Learning']}
+           'hard':['pattern','algorithms','malicious','optical']}
 
 choices = []
 
-#-----------#
-#-Functions-#
-#-----------#
-
+#Functions
 def win(guesses):
-    #---Function if Player Wins the Game---#
-    #Input: Number of tries remaining
-    #Output: The you won the game message
-    winning = "***You won with " + str(guesses) + " guesses remaining!***"
+
+    #If Player Wins the Game
+    winning = " You won with " + str(guesses) + " guesses remaining! "
     return winning
 
 def right(choice_index,level,guesses):
-    #---Function if Player Chooses Right---#
-    #Inputs: Which answer user is on, level, and number of tries remaining
-    #Outputs: Choice function with a reduced number of tries remaining
-    correct_answer = """
-***Correct! '""" + str(choices[choice_index]) + "' is the right choice!***"
+
+    #If Player Chooses Right
+    correct_answer = """ Correct! '""" + str(choices[choice_index]) + "' is the right choice! "
     print correct_answer
     quiz_text[level] = quiz_text[level].replace(holders[choice_index],choices[choice_index])
     return choice(level,choice_index+1,guesses)
 
 def wrong(guesses,level,choice_index):
-    #---Function if Player Chooses Wrong---#
-    #Inputs: Number of tries remaining, level, and which answer user is on
-    #Outputs: Game Over Message, or Wrong Answer Message
+
+    #If Player Chooses Wrong
     wrong_choice = choices.pop()
     if guesses == 0:
-        game_over = "***You've chosen wrong too many times!  Game over!****"
+        game_over = " You've chosen wrong too many times! Game over! "
         return game_over
     else:
         guesses -= 1
-        wrong_answer_text = """***'""" + wrong_choice + """' isn't the correct answer!***
-***Let's try again; you have """ + str(guesses) + ' trys left!***'
+        wrong_answer_text = """ '""" + wrong_choice + """' isn't the correct answer!
+Try again; you have """ + str(guesses) + ' trys left!'
+
         print wrong_answer_text
         return choice(level,choice_index,guesses)
 
 def choice(level,choice_index,guesses):
-    #---Function to Prompt User for an Answer---#
-    #Inputs: level, which question the user is on, and how many tries remain
-    #Outputs: Winning Message, Correct Answer Message, Wrong Answer Message, or Game Over Message
+
+    #Prompt User for an Answer
     print quiz_text[level]
     if choice_index == len(answers[level]):
         return win(guesses)
@@ -121,35 +91,30 @@ def choice(level,choice_index,guesses):
         return wrong(guesses,level,choice_index)
 
 def start(level):
-    guesses_prompt = """***Please select the number of guesses you'd like***"""
+
+    #Choosing guesses according to level
+    guesses_prompt = """ Please select the number of guesses you'd like """
     guesses = raw_input(guesses_prompt)
     if int(guesses):
         guesses = int(guesses)
-        guess_text = """
-***You will get '""" + str(guesses) + " trys!***"
+        guess_text = """ You will get '""" + str(guesses) + " trys! "
         print guess_text
         return choice(level.lower(),0,guesses)
 
 
 def quiz():
-    #---Overarching Quiz Function---#
-    user_prompt = """
-***Please select a game difficulty by typing it in!***
-***Possible choices include easy, medium, and hard***
-"""
+
+    #Quiz Function
+    user_prompt = """ Please select a game difficulty by typing it in!
+Possible choices include easy, medium, and hard """
     level = raw_input(user_prompt)
-    intro = """
-***You've chosen """ + str(level) + "!***"
+    intro = """ You've chosen """ + str(level) + "!"
     print intro
     if level.lower() == 'hard' or level.lower() == 'medium' or level.lower() == 'easy':
         return start(level)
     else:
-        wrong_level = """
-***'""" + level + "' is not a level option!***"
+        wrong_level = """ '""" + level + "' is not a level option! "
         print wrong_level
         return quiz()
 
-#-----------------#
-#-Running Program-#
-#-----------------#
 print quiz()
